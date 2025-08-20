c665f19e543056bac5be090e9a6033a966817c5c
mal <malcolm.sharman@microba.com>
2025-08-20 19:21:40 +1000

    re-org config

--- custom.el
+++ custom.el
@@ -22,20 +22,3 @@
         [?\e] `(menu-item "" ,esc-binding :filter personal/-tty-ESC-filter)))))
 
 (personal/catch-tty-ESC)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(warning-suppress-types
-   '(((copilot copilot-no-mode-indent))
-     ((copilot copilot-no-mode-indent))
-     ((copilot copilot-no-mode-indent))
-     (defvaralias))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(require 'org-protocol)
